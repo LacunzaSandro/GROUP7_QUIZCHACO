6fadfc4b329ade8b0ce23a73c3d6f79d3802b3e2
Sandro Lacunza <sandrolacunza@gmail.com>
2021-08-23 14:56:19 -0300

    Login y Register

---
+++ QUIZCHACO/Apps/authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # authentication
+    path('login', views.Login, name='login'),
+    path('register', views.Register, name='register'),
+]
